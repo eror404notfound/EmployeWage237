e697275608a9ee36b808d6d36d43aa20dabe7678
Ankit <ankitcool4370@gmail.com>
2022-12-26 21:07:57 +0530

    Performing UC 4

--- EmployeeWage237/Program.cs
+++ EmployeeWage237/Program.cs
@@ -7,31 +7,35 @@ using System.Threading.Tasks;
 namespace EmployeeWage237
 {
     internal class Program
-    {
+    { 
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Employee Wage Calculation");
-
-            int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
+            Console.WriteLine("Welcome to EmployeeWage Computation");
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
             {
-                empHrs = 8;
+                case IS_FULL_TIME:
+                    Console.WriteLine("Full Time Employee is Present");
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    Console.WriteLine("Part Time Employee is Present");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    empHrs = 0;
+                    break;
             }
-            else
-            {
-                empHrs = 0;
-            }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
+            empWage = EMP_RATE_PER_HOUR * empHrs;
+            Console.WriteLine("Employee Wage : " + empWage);
 
-
-
-            Console.ReadLine();
             Console.ReadLine();
         }
     }

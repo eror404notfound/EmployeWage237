f41bf96c96b8fb9023ca73b1c8451348737e773c
Ankit <ankitcool4370@gmail.com>
2022-12-26 20:40:01 +0530

    Welcome to the Employee Wage

---
+++ EmployeeWage237/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage237
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage");
+            int IS_FULL_TIME = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
